6b4d251de7632bbaec594b3029299edbbcceb366
gabriosilva <gabrio.lina@hotmail.com>
2022-04-26 20:44:05 -0300

    chore(semana2): add semana2 files

--- 02_TUTORIAL/Semana 1/app_11.js
+++ 02_TUTORIAL/Semana 1/app_11.js
@@ -1,16 +1,21 @@
-const http = require('http');
-const hostname = '127.0.0.1';
+const http = require("http");
+const hostname = "127.0.0.1";
 const port = 3011;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<!DOCTYPE html> <head> <title>Etapa 0 - INSTALACAO</title></head> \
+  res.setHeader("Content-Type", "text/html");
+  res.end(
+    '<!DOCTYPE html> <head> <title>Etapa 0 - INSTALACAO</title></head> \
               <body>\
                 <div id="main"> \
-                       <h1> Etapa 1 - INSTALACAO - Servidor Node.js </h1> \
-                       <H2> Meu servidor NODE.js funciona!</H2> </div> \
+                      <h1>Gabrio Lina</h1> \
+                      <p>Fullname: Gabrio Lina da Silva</p>\
+                      <p> Phone: +55 62 98214-5560 </p> \
+                      <p> Email: gabriosilva123@gmail.com </p> \
+                      </div> \
               </body> \
-           </html>');
+          </html>'
+  );
 });
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);

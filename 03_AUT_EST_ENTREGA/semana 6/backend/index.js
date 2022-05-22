9b0b60e1ea41e80aad030ba00c81781027d2ceb3
gabriosilva <gabrio.lina@hotmail.com>
2022-05-22 20:56:28 -0300

    chore(exercise): add curriculum endpoints

---
+++ 03_AUT_EST_ENTREGA/semana 6/backend/index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const app = express();
+
+// server settings
+
+// listen in all network cards (access it at localhost:80)
+const hostname = "0.0.0.0";
+const port = 80;
+
+// loads sqlite3 database
+const sqlite3 = require("sqlite3").verbose();
+const DBPATH = "database.db";
+
+// shows frontend
+app.use(express.static("../frontend/"));
+
+// use json as middleware
+app.use(express.json());
+
+// endpoints
+app.get("/workExperience", (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  let db = new sqlite3.Database(DBPATH);
+  let sql = "SELECT * FROM workExperience ORDER BY startDate";
+  let params = [];
+
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      throw err;
+    }
+
+    // response
+    res.json({ workExperiences: rows });
+  });
+  db.close();
+});
+
+app.get("/studyExperience", (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  let db = new sqlite3.Database(DBPATH);
+  let sql = "SELECT * FROM studyExperience ORDER BY startDate";
+  let params = [];
+
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      throw err;
+    }
+
+    // response
+    res.json({ studyExperiences: rows });
+  });
+  db.close();
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});

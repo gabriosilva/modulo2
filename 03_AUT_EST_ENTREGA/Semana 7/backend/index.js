d73feec70121fade93f27f352dff7487d35987a1
gabriosilva <gabrio.lina@hotmail.com>
2022-05-30 14:26:24 -0300

    chore(exercise): include new CRUD endpoints

---
+++ 03_AUT_EST_ENTREGA/Semana 7/backend/index.js
@@ -0,0 +1,213 @@
+const express = require("express");
+const app = express();
+
+// server settings
+
+// listen in all network cards (access it at localhost:80)
+const hostname = "0.0.0.0";
+const port = 80;
+
+// loads sqlite3 database
+const sqlite3 = require("sqlite3").verbose();
+const DBPATH = "database.db";
+
+// shows frontend
+app.use(express.static("../frontend/"));
+
+// use json as middleware
+app.use(express.json());
+
+// endpoints
+
+// returns the workExperience list
+app.get("/workExperience", (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  let db = new sqlite3.Database(DBPATH);
+  let sql = "SELECT * FROM workExperience ORDER BY startDate";
+  let params = [];
+
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      throw err;
+    }
+
+    // response
+    res.json({ workExperiences: rows });
+  });
+  db.close();
+});
+
+// returns the workExperience list
+app.post("/workExperience/add", (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  let db = new sqlite3.Database(DBPATH);
+  let sql =
+    "INSERT INTO workExperience(title, workType, startDate, endDate, logoPath) VALUES(?,?,?,?,?)";
+
+  // define query params
+  let params = [];
+  let { title, workType, startDate, endDate, logoPath } = req.body;
+  params.push(title, workType, startDate, endDate, logoPath);
+
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      throw err;
+    }
+
+    // response
+    res.json(rows);
+  });
+  db.close();
+});
+
+// returns the workExperience list
+app.post("/workExperience/update", (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  let db = new sqlite3.Database(DBPATH);
+  let sql =
+    "UPDATE workExperience SET title=?, workType=?, startDate=?, endDate=?, logoPath=? WHERE id=?";
+
+  // define query params
+  let params = [];
+  let { title, workType, startDate, endDate, logoPath, id } = req.body;
+  params.push(title, workType, startDate, endDate, logoPath, id);
+
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      throw err;
+    }
+
+    // response
+    res.json(rows);
+  });
+  db.close();
+});
+
+// returns the workExperience list
+app.post("/workExperience/delete", (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  let db = new sqlite3.Database(DBPATH);
+  let sql = "DELETE FROM workExperience WHERE id=?";
+
+  // define query params
+  let params = [];
+  params.push(req.body.id);
+
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      throw err;
+    }
+
+    // response
+    res.json(rows);
+  });
+  db.close();
+});
+
+// StudyExperience API
+
+// returns the studyExperience list
+app.get("/studyExperience", (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  let db = new sqlite3.Database(DBPATH);
+  let sql = "SELECT * FROM studyExperience ORDER BY startDate";
+  let params = [];
+
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      throw err;
+    }
+
+    // response
+    res.json({ studyExperiences: rows });
+  });
+  db.close();
+});
+
+// starts the server
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+// returns the workExperience list
+app.post("/studyExperience/add", (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  let db = new sqlite3.Database(DBPATH);
+  let sql =
+    "INSERT INTO studyExperience(title, studyType, startDate, endDate, logoPath) VALUES(?,?,?,?,?)";
+
+  // define query params
+  let params = [];
+  let { title, studyType, startDate, endDate, logoPath } = req.body;
+  params.push(title, studyType, startDate, endDate, logoPath);
+
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      throw err;
+    }
+
+    // response
+    res.json(rows);
+  });
+  db.close();
+});
+
+// returns the workExperience list
+app.post("/studyExperience/update", (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  let db = new sqlite3.Database(DBPATH);
+  let sql =
+    "UPDATE studyExperience SET title=?, studyType=?, startDate=?, endDate=?, logoPath=? WHERE id=?";
+
+  // define query params
+  let params = [];
+  let { title, studyType, startDate, endDate, logoPath, id } = req.body;
+  params.push(title, studyType, startDate, endDate, logoPath, id);
+
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      throw err;
+    }
+
+    // response
+    res.json(rows);
+  });
+  db.close();
+});
+
+// returns the workExperience list
+app.post("/studyExperience/delete", (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  let db = new sqlite3.Database(DBPATH);
+  let sql = "DELETE FROM studyExperience WHERE id=?";
+
+  // define query params
+  let params = [];
+  params.push(req.body.id);
+
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      throw err;
+    }
+
+    // response
+    res.json(rows);
+  });
+  db.close();
+});
---
+++ 03_AUT_EST_ENTREGA/Semana 7/frontend/script.js
@@ -0,0 +1,98 @@
+let workCards = [
+  {
+    title: "Preview",
+    type: "Preview",
+    duration: "Abr de 2022 - abr de 2022",
+    logo: "./assets/brasaHacksLogo.png",
+  },
+];
+
+let studyCards = [
+  {
+    title: "Preview",
+    type: "Preview",
+    duration: "Abr de 2022 - abr de 2022",
+    logo: "./assets/brasaHacksLogo.png",
+  },
+];
+
+function cardFactory(title, type, duration, logo) {
+  return `<div class="card work-card col-md-2" id="card${title}">
+  <img class="card-img-top" src="${logo}">
+  <div class="card-body">
+    <p class="card-text">${title} · ${type}</p>
+    <small class="text-muted">${duration}</small>
+  </div>
+</div>`;
+}
+
+async function fetchStudyCards() {
+  const responseRaw = await fetch("/studyExperience");
+  const responseJson = await responseRaw.json();
+  studyCards = responseJson.studyExperiences;
+  return studyCards;
+}
+
+async function fetchWorkCards() {
+  const responseRaw = await fetch("/workExperience");
+  const responseJson = await responseRaw.json();
+  workCards = responseJson.workExperiences;
+  return workCards;
+}
+
+function parseCardDate(startDate, endDate) {
+  const months = [
+    "jan",
+    "fev",
+    "mar",
+    "abr",
+    "mai",
+    "jun",
+    "jul",
+    "ago",
+    "set",
+    "out",
+    "nov",
+    "dec",
+  ];
+  console.log(endDate)
+  console.log(startDate, endDate);
+  return `${months[startDate.getMonth()]} de ${startDate.getFullYear()} - ${
+    months[endDate.getMonth()]
+  } de ${endDate.getFullYear()}`;
+}
+
+// Appends a new card in the "Work" section
+$(document).ready(function () {
+  fetchWorkCards().then((workExperiences) => {
+    workExperiences.forEach((workCard) => {
+      let duration = parseCardDate(
+        new Date(workCard.startDate),
+        new Date(workCard.endDate)
+      );
+      let newCard = cardFactory(
+        workCard.title,
+        workCard.workType,
+        duration,
+        workCard.logoPath
+      );
+      $("#workCardBox").append(newCard);
+    });
+  });
+
+  fetchStudyCards().then((studyExperiences) => {
+    studyExperiences.forEach((studyCard) => {
+      let duration = parseCardDate(
+        new Date(studyCard.startDate),
+        new Date(studyCard.endDate)
+      );
+      let newCard = cardFactory(
+        studyCard.title,
+        studyCard.studyType,
+        duration,
+        studyCard.logoPath
+      );
+      $("#studyCardBox").append(newCard);
+    });
+  });
+});

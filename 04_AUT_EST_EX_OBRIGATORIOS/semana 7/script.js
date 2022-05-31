bb58b19ca6fd3cd7f188545de10de2fee0eece9f
gabriosilva <gabrio.lina@hotmail.com>
2022-05-30 23:03:23 -0300

    chore(exercise): add 3 new exercises

---
+++ 04_AUT_EST_EX_OBRIGATORIOS/semana 7/script.js
@@ -0,0 +1,50 @@
+function increaseInput(inputId) {
+  const htmlElement = document.getElementById(inputId);
+  let value = parseInt(htmlElement.value);
+  const newValue = value + 1;
+  htmlElement.value = newValue;
+}
+
+function decreaseInput(inputId) {
+  const htmlElement = document.getElementById(inputId);
+  let value = parseInt(htmlElement.value);
+  const newValue = value > 0 ? value - 1 : 0;
+  htmlElement.value = newValue;
+}
+
+function swapInputs(inputAid, inputBid) {
+  const elementA = document.getElementById(inputAid);
+  const elementB = document.getElementById(inputBid);
+  const elementAvalue = elementA.value;
+  elementA.value = elementB.value;
+  elementB.value = elementAvalue;
+}
+
+function validateNumber(phoneNumber) {
+  let re = /^\(?(\d{2})\){1}[\- ]?(\d{5})[- ]{1}(\d{4})$/;
+  return re.test(phoneNumber);
+}
+
+function plusHandler() {
+  increaseInput("numberInput");
+}
+
+function minusHandler() {
+  decreaseInput("numberInput");
+}
+
+function swapHandler() {
+  swapInputs("numberInputA", "numberInputB");
+}
+
+function changeElementText(elementId, content) {
+  document.getElementById(elementId).textContent = content;
+}
+
+function validatePhoneHandler() {
+  const phoneNumber = document.getElementById("numberInputPhone").value;
+  const isValid = validateNumber(phoneNumber);
+  isValid
+    ? changeElementText("phoneValidationResult", "Número Válido")
+    : changeElementText("phoneValidationResult", "Número Inválido");
+}

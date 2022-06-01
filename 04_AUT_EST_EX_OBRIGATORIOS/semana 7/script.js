cd478ebee5f1fe5eb22949b5ddf22aa931789e5a
gabriosilva <gabrio.lina@hotmail.com>
2022-05-31 22:34:50 -0300

    refact(exercise): divide exercise pages and include exercise 5

--- 04_AUT_EST_EX_OBRIGATORIOS/semana 7/script.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/semana 7/script.js
@@ -89,3 +89,178 @@ function flightPurchaseHandler() {
 }
 
 // -------------------------------------
+
+// exercise 5---------------------------
+let studentList = [];
+
+let examGrades = [];
+let homeworkGrades = [];
+let studentGrades = [];
+
+let gradeObj = {
+  averageGrade: 0,
+  averageExamGrades: 0,
+  averageHomeworkGrades: 0,
+  smallestExamGrade: 0,
+  biggestExamGrade: 0,
+  smallestHomeworkGrade: 0,
+  biggestHomeworkGrade: 0,
+};
+
+class Student {
+  constructor(id, name, homeworkGrade, examGrade, averageGrade) {
+    this.id = id;
+    this.name = name;
+    this.homeworkGrade = homeworkGrade;
+    this.examGrade = examGrade;
+    this.averageGrade = averageGrade;
+  }
+}
+
+function studentFactory(studentAmount) {
+  studentList = [];
+  const studentIdArray = [...Array(studentAmount).keys()];
+  studentIdArray.forEach((studentId) => {
+    const studentObj = new Student(
+      studentId,
+      `Estudante-${studentId}`,
+      0,
+      0,
+      0
+    );
+    studentList.push(studentObj);
+  });
+  return studentList;
+}
+
+function generateStudentViewElement(studentObj) {
+  const tableContentElement = document.createElement("tr");
+  const htmlContent = `
+  <th scope="row">${studentObj.id}</th>
+  <td>${studentObj.name}</td>
+  <td>
+    <div class="row">
+      <span class="col-6"> <input type="text" class="form-control" aria-label="Default"
+          aria-describedby="inputGroup-sizing-default" id="studentHomeworkGrade${studentObj.id}"
+          placeholder="Valor"></span>
+    </div>
+  </td>
+  <td>
+    <div class="row">
+      <span class="col-6"> <input type="text" class="form-control" aria-label="Default"
+          aria-describedby="inputGroup-sizing-default" id="studentExamGrade${studentObj.id}"
+          placeholder="Valor"></span>
+    </div>
+  </td>   
+  <td><span id="averageGrade${studentObj.id}"> -- </span></td>
+  `;
+
+  tableContentElement.innerHTML = htmlContent;
+  return tableContentElement;
+}
+
+function calculateStudentAverageGrade(examGrade, homeworkGrade) {
+  return (examGrade * 2 + homeworkGrade * 3) / 5;
+}
+
+function processStudentGrades(studentsArray) {
+  examGrades = [];
+  homeworkGrades = [];
+  studentGrades = [];
+  studentsArray.map((student) => {
+    // gets the grade values
+    let homeworkGrade = parseInt(
+      document.getElementById(`studentHomeworkGrade${student.id}`).value
+    );
+    let examGrade = parseInt(
+      document.getElementById(`studentExamGrade${student.id}`).value
+    );
+
+    // calculates the student average
+    student.examGrade = examGrade;
+    student.homeworkGrade = homeworkGrade;
+
+    student.averageGrade = calculateStudentAverageGrade(
+      examGrade,
+      homeworkGrade
+    );
+
+    // loads the grades to the list
+    examGrades.push(examGrade);
+    homeworkGrades.push(homeworkGrade);
+    studentGrades.push(student.averageGrade);
+
+    document.getElementById(`averageGrade${student.id}`).textContent =
+      student.averageGrade;
+  });
+}
+
+function processAndShowGeneralGrades() {
+  examGrades.sort();
+  homeworkGrades.sort();
+  studentGrades.sort();
+  let result = {
+    averageGrade: 0,
+    averageExamGrade: 0,
+    averageHomeworkGrade: 0,
+    lowestExamGrade: examGrades[0],
+    highestExamGrade: examGrades[examGrades.length - 1],
+    lowestHomeworkGrade: homeworkGrades[0],
+    highestHomeworkGrade: homeworkGrades[homeworkGrades.length - 1],
+  };
+
+  result.averageExamGrade =
+    examGrades.reduce((a, b) => {
+      return a + b;
+    }) / examGrades.length;
+
+  result.averageHomeworkGrade =
+    homeworkGrades.reduce((a, b) => {
+      return a + b;
+    }) / homeworkGrades.length;
+  result.averageGrade =
+    studentGrades.reduce((a, b) => {
+      return a + b;
+    }) / studentGrades.length;
+
+  // sets the html elements values accordingly
+  document.getElementById("averageGradeResult").textContent =
+    result.averageGrade;
+  document.getElementById("averageExamGradeResult").textContent =
+    result.averageExamGrade;
+  document.getElementById("averageHomeworkGradeResult").textContent =
+    result.averageHomeworkGrade;
+  document.getElementById("highestExamGradeResult").textContent =
+    result.highestExamGrade;
+  document.getElementById("lowestExamGradeResult").textContent =
+    result.lowestExamGrade;
+  document.getElementById("highestHomeworkGradeResult").textContent =
+    result.highestHomeworkGrade;
+  document.getElementById("lowestHomeworkGeralResult").textContent =
+    result.lowestHomeworkGrade;
+}
+
+function appendStudentsToTableBody(studentsArray, tableBodyId) {
+  const tableBody = document.getElementById(tableBodyId);
+  tableBody.innerHTML = "";
+  // iterates over the students object array
+  studentsArray.forEach((studentObj) => {
+    const studentElement = generateStudentViewElement(studentObj);
+    tableBody.append(studentElement);
+  });
+}
+
+function addStudentsHandler() {
+  const studentAmount = parseInt(
+    document.getElementById("studentAmountInput").value || 0
+  );
+  studentFactory(studentAmount);
+  appendStudentsToTableBody(studentList, "studentTableBody");
+}
+
+function processStudentsHandler() {
+  processStudentGrades(studentList);
+  processAndShowGeneralGrades();
+}
+
+// -------------------------------------
